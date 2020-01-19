832577a74aec3a100ae56c558bfc8eb089ad9125
Artur Pawlak <pawlak.artur90@gmail.com>
2020-01-19 20:36:24 +0100

    Update nestjs project with mongodb

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,8 +1,14 @@
-export class Product {
-  constructor(
-    public id: string,
-    public title: string,
-    public description: string,
-    public price: number,
-  ) {}
+import * as mongoose  from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: false },
+  description: { type: String, required: false },
+  price: { type: Number, required: false },
+});
+
+export interface Product extends mongoose.Document {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
 }
